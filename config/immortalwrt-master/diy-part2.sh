f4b4e34be6e78f69604b1b5ceb9950d0a41651e0
QBi <95951192+mybeXX@users.noreply.github.com>
2026-01-28 22:31:23 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -8,8 +8,8 @@
 
 # ------------------------------- Main source started -------------------------------
 #
-# 修改默认密码为 'password'
-sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+# 设置 root 用户密码为空
+sed -i 's/root:::0:99999:7:::/root::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # 设置 etc/openwrt_release (自定义版本显示)
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
@@ -26,7 +26,7 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 
 # 添加 TurboACC 插件
-# 该脚本会自动下载源码、处理依赖关系并修补相关文件以适配 fw4/fw3
+# 该脚本会自动下载源码、处理依赖并适配防火墙环境
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # ------------------------------- Other ends -------------------------------
